fa68187668f64def8ed657b6dac8fdc4acb65ee3
willianalvesp95-web <willianalvesp95@gmail.com>
2026-05-25 18:06:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,5 +40,32 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 
 });
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ticket")
+    .setDescription("Abrir ticket"),
+
+  new SlashCommandBuilder()
+    .setName("comprar")
+    .setDescription("Ver loja")
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+client.once("ready", async () => {
+  console.log(`Bot ligado: ${client.user.tag}`);
+
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("Comandos registrados!");
+  } catch (err) {
+    console.error(err);
+  }
+});
 client.login(process.env.DISCORD_TOKEN);
