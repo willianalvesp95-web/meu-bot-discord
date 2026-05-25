f3bef1a7bfb6a04562391047ffd93a5ab00a2b99
willianalvesp95-web <willianalvesp95@gmail.com>
2026-05-25 17:53:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,42 @@
 const { 
   Client, 
   GatewayIntentBits, 
-  Events,
-  REST,
-  Routes,
-  SlashCommandBuilder
+  Events 
 } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-// comandos
-const commands = [
-  new SlashCommandBuilder()
-    .setName("teste")
-    .setDescription("teste"),
-
-  new SlashCommandBuilder()
-    .setName("comprar")
-    .setDescription("Ver loja")
-].map(cmd => cmd.toJSON());
-
-const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
-
-client.once("ready", async () => {
+// BOT LIGANDO
+client.once("ready", () => {
   console.log(`Bot ligado: ${client.user.tag}`);
-
-  try {
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
-
-    console.log("Comandos registrados!");
-  } catch (err) {
-    console.error(err);
-  }
 });
 
-// respostas
+// COMANDOS
 client.on(Events.InteractionCreate, async (interaction) => {
 
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "ticket") {
-    return interaction.reply("🎫 Ticket criado!");
-  }
+  try {
 
-  if (interaction.commandName === "comprar") {
-    return interaction.reply("💰 Loja aberta!");
+    if (interaction.commandName === "ticket") {
+      return await interaction.reply("🎫 Ticket criado!");
+    }
+
+    if (interaction.commandName === "comprar") {
+      return await interaction.reply("💰 Loja aberta!");
+    }
+
+  } catch (err) {
+    console.error(err);
+
+    if (interaction.replied || interaction.deferred) return;
+
+    await interaction.reply({
+      content: "❌ Erro ao executar o comando.",
+      ephemeral: true
+    });
   }
 
 });
